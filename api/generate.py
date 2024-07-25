0575f744bf86de36723e7aac97ffdfbf207a141b
Avelino Figueira <avesousa14@gmail.com>
2024-07-25 12:33:23 -0300

    Add logs

--- api/generate.py
+++ api/generate.py
@@ -1,12 +1,16 @@
 import json
 import os
 import threading
+import logging
 import psycopg2
 from solders.pubkey import Pubkey
 from solders.keypair import Keypair
 from base58 import b58encode
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -23,36 +27,41 @@ def generate_vanity_address(suffix: str):
         secret = b58encode(keypair.secret()).decode('utf-8')
         # print(vanity_address)
         if vanity_address.endswith(suffix):
+            logger.info(f"Wallet generated: ${vanity_address}")
             save_to_database(vanity_address, secret)
-            print("Save wallet")
-            print(vanity_address)
 
 def save_to_database(public_key, secret_key):
-    postgres_url = os.getenv("POSTGRES_URL")
-    endpoint_id = "ep-falling-paper-a4a6awfy"  # reemplaza esto con tu endpoint ID real
-    postgres_url += f"&options=endpoint%3D{endpoint_id}"
-    
-    conn = psycopg2.connect(postgres_url)
-    cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO vanity_addresses (public_key, secret_key) VALUES (%s, %s)",
-        (public_key, secret_key)
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        postgres_url = os.getenv("POSTGRES_URL")
+        endpoint_id = "ep-falling-paper-a4a6awfy"  # reemplaza esto con tu endpoint ID real
+        postgres_url += f"&options=endpoint%3D{endpoint_id}"
+        
+        conn = psycopg2.connect(postgres_url)
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO vanity_addresses (public_key, secret_key) VALUES (%s, %s)",
+            (public_key, secret_key)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+        logger.info(f"Saved address {public_key} to database")
+    except Exception as e:
+        logger.error(f"Error saving to database {e}")
 
 @app.route('/start', methods=['GET'])
 def start_generation():
     suffix = request.args.get('suffix', 'pump')
     stop_event.clear()
     threading.Thread(target=generate_vanity_address, args=(suffix,)).start()
+    logger.info(f"Started generation process with {suffix} like suffix")
     return jsonify({"status": "started", "suffix": suffix})
 
 @app.route('/stop', methods=['GET'])
 def stop_generation():
     stop_event.set()
+    logger.info("Stopped generation process")
     return jsonify({"status": "stopped"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True)
