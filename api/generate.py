9fb2c3a54b4254b84ea1e0b2afd665b25f5c58fb
Avelino Figueira <avesousa14@gmail.com>
2024-07-25 12:22:40 -0300

    Update save db

--- api/generate.py
+++ api/generate.py
@@ -6,6 +6,9 @@ from solders.pubkey import Pubkey
 from solders.keypair import Keypair
 from base58 import b58encode
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 stop_event = threading.Event()
@@ -18,24 +21,22 @@ def generate_vanity_address(suffix: str):
         keypair = Keypair()
         vanity_address = b58encode(str(keypair.pubkey())).decode('utf-8')
         secret = b58encode(keypair.secret()).decode('utf-8')
-        print(vanity_address)
+        # print(vanity_address)
         if vanity_address.endswith(suffix):
-            save_to_database(vanity_address, keypair.pubkey(), secret)
+            save_to_database(vanity_address, secret)
             print("Save wallet")
             print(vanity_address)
 
-def save_to_database(vanity_address, public_key, secret_key):
-    conn = psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
+def save_to_database(public_key, secret_key):
+    postgres_url = os.getenv("POSTGRES_URL")
+    endpoint_id = "ep-falling-paper-a4a6awfy"  # reemplaza esto con tu endpoint ID real
+    postgres_url += f"&options=endpoint%3D{endpoint_id}"
+    
+    conn = psycopg2.connect(postgres_url)
     cur = conn.cursor()
     cur.execute(
-        "INSERT INTO vanity_addresses (vanity_address, public_key, secret_key) VALUES (%s, %s, %s)",
-        (vanity_address, public_key.to_base58().decode('utf-8'), b58encode(secret_key).decode('utf-8'))
+        "INSERT INTO vanity_addresses (public_key, secret_key) VALUES (%s, %s)",
+        (public_key, secret_key)
     )
     conn.commit()
     cur.close()
@@ -54,4 +55,4 @@ def stop_generation():
     return jsonify({"status": "stopped"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
